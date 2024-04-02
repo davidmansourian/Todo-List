1bbcb17093dbbe615effb85a3c616adeac6d2a73
David Mansourian <mada20sv@student.ju.se>
2024-04-02 17:27:47 +0200

    Added line-per-line description for assignment

--- iOSTodoListThesis/Models/TodoTaskManager.swift
+++ iOSTodoListThesis/Models/TodoTaskManager.swift
@@ -26,17 +26,28 @@ final class TodoTaskManager {
         fetchTodoData()
     }
     
+    // MARK: Line-per-line description
+    /// - Initialize a task with name
+    /// - insert into modelContext (database)
+    /// - update class variable 'tasks' by calling fetchTodoData()
     public func addTodoTask(name: String) {
         let task = TodoTask(name: name, isCompleted: false, timestamp: Date.now)
         modelContext.insert(task)
         fetchTodoData()
     }
     
+    // MARK: Line-per-line description
+    /// - Delete TodoTask from modelContext
+    /// - update class variable 'tasks' by calling fetchTodoData()
     public func deleteTodoTask(_ todoTask: TodoTask) {
         modelContext.delete(todoTask)
         fetchTodoData()
     }
     
+    // MARK: Line-per-line description
+    /// - Toggle completion state for task
+    /// - Set the "completedOn" variable for the specific task if the task isCompleted
+    /// - update class variable 'tasks' by calling fetchTodoData()
     public func toggleCompletion(_ todoTask: TodoTask) {
         todoTask.isCompleted.toggle()
         
@@ -45,6 +56,12 @@ final class TodoTaskManager {
         fetchTodoData()
     }
     
+    // MARK: Line-per-line description
+    /// - Loop through class variable 'tasks'
+    /// - Check if completedOn exists
+    /// - If completedOn exists, calculate day difference between "today" and completedOn
+    /// - If dayDifference is greater than or equal to class variable 'daysUntilDeletion', call deleteTodoTask(_ todoTask: TodoTask), else do nothing
+    /// - update class variable 'tasks' by calling fetchTodoData()
     public func clearCompletedTodoTasks() {
         for task in tasks {
             if let completedDate = task.completedOn,
@@ -56,6 +73,12 @@ final class TodoTaskManager {
         fetchTodoData()
     }
     
+    // MARK: Line-per-line description
+    /// - Start do-catch block
+    /// - Create a descriptor that fetched 'TodoTask' from the datbase with sorting criteria for completed and timestamp (creation date)
+    /// - set class variable 'tasks' to the sorted fetched data from modelContext
+    /// - If necessary, enter catch
+    /// - Print custom message if error in catch
     private func fetchTodoData() {
         do {
             let descriptor = FetchDescriptor<TodoTask>(sortBy: [SortDescriptor(\.isCompleted, order: .forward), SortDescriptor(\.timestamp)])
